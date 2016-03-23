9c47353696c649bec533190bacbb487357abf385
cherry324 <cherry324@missionrnd.com>
2016-03-24 03:03:04 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -14,7 +14,59 @@ NOTES:
 There are better ways of solving the problem than a brute-force solution which is of O(n^2)
 complexity .
 */
+#include<stdlib.h>
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+int partition(int arr[], int l, int h)
+{
+	int x = arr[h];
+	int i = (l - 1);
 
-int findSingleOccurenceNumber(int *A, int len) {
-	return -1;
+	for (int j = l; j <= h - 1; j++)
+	{
+		if (arr[j] <= x)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[h]);
+	return (i + 1);
+}
+
+
+void quickSort(int A[], int l, int h)
+{
+	if (l < h)
+	{
+
+		int p = partition(A, l, h);
+		quickSort(A, l, p - 1);
+		quickSort(A, p + 1, h);
+	}
+}
+int findSingleOccurenceNumber(int *A, int len)
+{
+	if (A == NULL || len < 0)
+		return -1;
+	quickSort(A, 0, len - 1);/*sorting the array using quick sort algorithm*/
+	int x=A[0];                    
+	for (int i = 1; i < len; i++)
+	{
+		if (i ==(len - 1))
+		{
+			if (A[i - 1] != A[i])
+				x = A[i];
+			continue;
+		}
+		if (A[i + 1] != A[i] && A[i - 1] != A[i])/*checking the predessor and successor 
+												   the current element*/
+			x = A[i];
+
+	}
+	return x;
 }
